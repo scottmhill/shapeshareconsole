4db180c1d9a7e56b504f8509c8ac78f0f266ade3
scottmhill <scottmwhill@gmail.com>
2014-03-18 12:17:18 -0500

    solutiond files added

---
+++ ShapeSharingConsolePush/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace ShapeSharingConsolePush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:58607";
+
+            //string hubName = "ShareHub";
+            string hubName = "Test";
+
+            var hubConnection = new HubConnection(url);
+
+            IHubProxy hub = hubConnection.CreateHubProxy(hubName);
+
+            hubConnection.Start().ContinueWith(task1 =>
+            {
+                double x = 1;
+                double direction = 1;
+                bool running = true;
+
+                while (running)
+                {
+                    x += direction;
+
+                    if (x > 100 || x < 1) direction = - direction;
+
+                    hub.Invoke("ShareCoordinates", x, 50.0).ContinueWith(task2 =>
+                        {
+                            if (task2.IsFaulted)
+                            {
+                                running = false;
+                                Console.WriteLine("Error: " + task2.Exception.ToString());
+                                Console.ReadLine();
+                            }
+                        });
+                    System.Threading.Thread.Sleep(5);
+                }
+
+            }).Wait();
+        }
+    }
+}
---
+++ ShapeSharingConsoleSubscribe/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using System.Windows.Threading;
+
+namespace ShapeSharingConsoleSubscribe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:58607/";
+
+            //string hubName = "ShareHub";
+            string hubName = "Test";
+
+            var hubConnection = new HubConnection(url);
+
+            IHubProxy hub = hubConnection.CreateHubProxy(hubName);
+
+            int indexCounter = 0;
+
+            //Subscribe to a hub message
+            hub.On<double, double>("MoveShape", (x, y) =>
+            {
+                Console.WriteLine(string.Format("Coordinates ({0}, {1}) [{2}]", x, y, indexCounter++));
+
+            });
+
+            hubConnection.Start().ContinueWith(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        Console.WriteLine("Error: " + task.Exception.ToString());
+                    }
+                }).Wait();
+
+            Console.ReadLine();
+        }
+    }
+}
